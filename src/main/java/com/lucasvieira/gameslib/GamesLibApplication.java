79fe062df867f6595273a9a106e2ed2a0e2f76fc
Lucas Gabriel Babolim Vieira <lucasvieira0420@gmail.com>
2024-11-11 13:10:49 -0300

    Project created

---
+++ src/main/java/com/lucasvieira/gameslib/GamesLibApplication.java
@@ -0,0 +1,13 @@
+package com.lucasvieira.gameslib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasvieira/gameslib/GamesLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasvieira.gameslib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
